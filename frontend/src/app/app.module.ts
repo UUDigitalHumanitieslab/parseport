e97a2f774f8731fbb60ac31b54b3f6cc665c55d0
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2024-04-18 23:10:11 +0200

    ProofPipe

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { HighlightPipe } from "./shared/pipes/highlight.pipe";
 import { SpindleAboutComponent } from './spindle/spindle-about/spindle-about.component';
 import { SpindleNotationComponent } from './spindle/spindle-notation/spindle-notation.component';
 import { ReferencesComponent } from './references/references.component';
+import { ProofPipe } from "./shared/pipes/proof.pipe";
 
 @NgModule({
     declarations: [
@@ -38,6 +39,7 @@ import { ReferencesComponent } from './references/references.component';
         ReferencesComponent,
         AethelComponent,
         HighlightPipe,
+        ProofPipe,
     ],
     imports: [
         AppRoutingModule,
---
+++ frontend/src/app/shared/pipes/proof.pipe.spec.ts
@@ -0,0 +1,43 @@
+import { TestBed } from "@angular/core/testing";
+import { ProofPipe } from "./proof.pipe";
+import { DomSanitizer } from "@angular/platform-browser";
+
+describe("ProofPipe", () => {
+    let pipe: ProofPipe;
+
+    beforeEach(async () => {
+        TestBed.configureTestingModule({
+            providers: [
+                {
+                    provide: DomSanitizer,
+                    useValue: {
+                        bypassSecurityTrustHtml: (value: string) => value,
+                    },
+                },
+            ],
+        });
+        const sanitizer = TestBed.inject(DomSanitizer);
+        pipe = new ProofPipe(sanitizer);
+    });
+
+    it("create an instance", () => {
+        expect(pipe).toBeTruthy();
+    });
+
+    it("should transform simple input", () => {
+        const input = "□app(NP⟶NP)";
+        const output = pipe.transform(input);
+        const expected =
+            '<span class="proof">□<sup>app</sup>(<span class="term">NP</span> ⊸ <span class="term">NP</span>)</span>';
+
+        expect(output).toEqual(expected);
+    });
+
+    it("should transform terms with uppercase and lowercase letters", () => {
+        const input = "Adjp";
+        const output = pipe.transform(input);
+        const expected = '<span class="proof"><span class="term">Adjp</span></span>';
+
+        expect(output).toEqual(expected);
+    });
+});

7360b279edcabff3845f065e0cd80aec06da50b7
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2024-05-24 15:33:04 +0200

    Update IsNonNull tests

--- frontend/src/app/shared/operators/IsNonNull.spec.ts
+++ frontend/src/app/shared/operators/IsNonNull.spec.ts
@@ -1,22 +1,31 @@
 import { filterOutNonNull } from "./IsNonNull";
 import { of } from "rxjs";
 
-fdescribe('isNonNull', () => {
+interface TestInterface {
+    myTest: 'always succeeds';
+}
+
+describe('isNonNull', () => {
     it('should filter out null and undefined values', () => {
-        const source = of('hello', null, 123, undefined, true, {}, []);
-        const expected = ['hello', 123, true, {}, []];
+        const testObject = { key: 'value' };
+        const testArray = [1, 2, 3];
+        const source = of('hello', null, 123, undefined, true, testObject, testArray);
+        const expected = ['hello', 123, true, testArray, testObject];
 
         source.pipe(filterOutNonNull()).subscribe((value) => {
             expect(expected.includes(value)).toBe(true);
         });
     });
 
-    it('should return correctly typed data', () => {
-        const source = of('hello');
-
+    it('should return data of the correct type', () => {
+        function testFunction(input: TestInterface): void {
+            expect(input.myTest).toBe('always succeeds');
+        }
+        const input: TestInterface = { myTest: 'always succeeds' };
+        const source = of(null, input, undefined);
         source.pipe(filterOutNonNull()).subscribe((value) => {
-            expect(typeof value).toBe('string');
-            expect(value).toBe('hello');
+            // This should not compile if null/undefined are allowed to pass through.
+            testFunction(value);
         });
     });
 });
